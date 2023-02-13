e8412174ed0f609c4437aa975fa232c9bc37d0d5
Nickon Babaei <babaein1@gmail.com>
2023-02-13 15:02:09 -0300

    created header and about component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+
+  return (
+    <header>
+      <nav>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/createRecipe'}>Create Recipe</Link>
+        <Link to={'/about'}>About</Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
