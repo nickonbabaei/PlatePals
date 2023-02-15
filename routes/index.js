76463965109c62395a489b8f2b4aaef8d8a5eb42
Nickon Babaei <babaein1@gmail.com>
2023-02-15 14:04:53 -0300

    linked delete router to controller

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.get('/', (req, res) => res.send('This is root!'))
 router.get('/recipes', Controller.getAllRecipes)
 router.post('/create-recipe', Controller.createRecipe)
 router.get('/read-recipe/:id', Controller.getRecipeById )
+router.delete('/delete-recipe', Controller.deleteRecipe)
 
 
 module.exports = router;
