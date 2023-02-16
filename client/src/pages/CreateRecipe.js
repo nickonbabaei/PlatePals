679bdc0e6d89a1a16121448d947cc96a7f4c6f0a
Nickon Babaei <babaein1@gmail.com>
2023-02-16 15:58:20 -0300

    added image url input

--- client/src/pages/CreateRecipe.js
+++ client/src/pages/CreateRecipe.js
@@ -16,59 +16,68 @@ const CreateRecipe = () => {
     const [recipeState, setRecipeState] = useState(initialRecipeState)
 
     const handleRecipeChange = (evt) => {
-        setRecipeState({... recipeState, [evt.target.id] : evt.target.value})
+        setRecipeState({ ...recipeState, [evt.target.id]: evt.target.value })
     }
 
     const handleRecipeSubmit = async (evt) => {
         evt.preventDefault()
-        await axios.post('http://localhost:3001/api/create-recipe', recipeState) 
+        await axios.post('http://localhost:3001/api/create-recipe', recipeState)
         setRecipeState(initialRecipeState)
         navigate('/')
     }
 
     return (
-      <div className="create-recipe">
-        <form onSubmit={handleRecipeSubmit}>
-            
-            <label htmlFor="name">Recipe Name:</label>
-            <input 
-            type="text" 
-            id="name" 
-            onChange={handleRecipeChange}
-            value={recipeState.name}
-            />
+        <div className="create-recipe">
+            <form onSubmit={handleRecipeSubmit}>
 
-            <label htmlFor="category">Category:</label>
-            <select id="category" onChange={handleRecipeChange} value={recipeState.category}>
-                <option value="63ebc0f1f3f976863fd609f4">Breakfast</option>                            
-                <option value="63ebc0f1f3f976863fd609f5">Lunch</option>
-                <option value="63ebc0f1f3f976863fd609f6">Dinner</option>
-                <option value="63ebc0f1f3f976863fd609f7">Snack</option>
-            </select>
+                <label htmlFor="name">Recipe Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    onChange={handleRecipeChange}
+                    value={recipeState.name}
+                />
 
-            <label htmlFor="ingredients">Ingredients Seperated by Commas:</label>
-            <textarea
-            id="ingredients"
-            cols="30"
-            rows="10"
-            onChange={handleRecipeChange}
-            value={recipeState.ingredients}
-            ></textarea>
+                <label htmlFor="category">Category:</label>
+                <select id="category" onChange={handleRecipeChange} value={recipeState.category}>
+                    <option value="63ebc0f1f3f976863fd609f4">Breakfast</option>
+                    <option value="63ebc0f1f3f976863fd609f5">Lunch</option>
+                    <option value="63ebc0f1f3f976863fd609f6">Dinner</option>
+                    <option value="63ebc0f1f3f976863fd609f7">Snack</option>
+                </select>
 
-            <label htmlFor="instructions">Instructions:</label>
-            <textarea
-            id="instructions"
-            cols="30"
-            rows="10"
-            onChange={handleRecipeChange}
-            value={recipeState.instructions}
-            ></textarea>
+                <label htmlFor="ingredients">Ingredients Seperated by Commas:</label>
+                <textarea
+                    id="ingredients"
+                    cols="30"
+                    rows="10"
+                    onChange={handleRecipeChange}
+                    value={recipeState.ingredients}
+                ></textarea>
 
-            <button type="submit">Create Recipe</button>
-        </form>
-       
-      </div>
+                <label htmlFor="instructions">Instructions:</label>
+                <textarea
+                    id="instructions"
+                    cols="30"
+                    rows="10"
+                    onChange={handleRecipeChange}
+                    value={recipeState.instructions}
+                ></textarea>
+
+                <label htmlFor="image">Image URL:</label>
+                <input
+                    type="text"
+                    id="image"
+                    placeholder="Not Required"
+                    onChange={handleRecipeChange}
+                    value={recievedRecipe.image}
+                />
+
+                <button type="submit">Create Recipe</button>
+            </form>
+
+        </div>
     )
-  }
-  
-  export default CreateRecipe
+}
+
+export default CreateRecipe
